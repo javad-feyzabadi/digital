c82c03170cc275acd45612e914296f3986617671
javad <javad.feyzabadi@yahoo.com>
2023-01-04 15:03:51 +0330

    create model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+class Category(models.models):
+    parent = models.ForeignKey(self,verbose_name='parent',blank=True,null=True,on_delete=models.CASCADE)
+    title = models.CharField(_('title'),max_length = 50)
+    description = models.TextField(_('description'),blank = True)
+    avatar = models.ImageField(_('avatar'),blank=True,upload_to='categories')
+    is_enable = models.BooleanField(_('is enable'),default=True)
+    created_time = models.DateTimeField(_('created time'),auto_created=True)
+    updated_time = models.DateTimeField(_('updated time'),auto_now=True)
 
-# Create your models here.
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+class Product(models.models):
+
+    pass
+
+class File(models.models):
+
+    pass
