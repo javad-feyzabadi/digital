ef10d16b912ee984fe48ca10c46e6029b3858ac8
javad <javad.feyzabadi@yahoo.com>
2023-01-07 17:14:00 +0330

    modified

--- products/views.py
+++ products/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
-def get(self):
-    pass
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from . models import Product
+from . serializers import ProductSerializers
+
+
+class ProductListView(APIView):
+
+    def get(self,request):
+        products = Product.objects.all()
+        serializer = ProductSerializers(products, many = True)
+        return Response(serializer.data)
