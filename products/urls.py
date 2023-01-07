a864cb832937a29664e6e6f66642a33febc8bc46
javad <javad.feyzabadi@yahoo.com>
2023-01-07 19:01:52 +0330

    edit url

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
 
-from . views import ProductListView
-
+from . views import (
+    ProductListView,ProductDetailView,
+    FileDetailView,FileListView,
+    CategoryListView,CategoryDetailView
+)
 
 urlpatterns = [
-    path('products/',ProductListView.as_view(),name='products'),
+    path('products/',ProductListView.as_view(),name='products_list'),
+    path('products/<int:pk>/',ProductDetailView.as_view(),name='products_detail'),
+
+    path('categories/',CategoryListView.as_view(),name='categories_list'),
+    path('categories/<int:pk>/',CategoryDetailView.as_view(),name='categories_detail'),
     
+    path('products/<int:product_id>/files/',FileListView.as_view(),name='file_list'),
+    path('products/<int:product_id>/files/<int:pk>/',FileDetailView.as_view(),name='file_detail'),
+    
+
 ]
 
