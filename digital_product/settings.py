98415d099b6472d5e0c2ea6b9ccbb3c2f96a011c
javad <javad.feyzabadi@yahoo.com>
2023-01-07 17:14:40 +0330

    modified drf settings

--- digital_product/settings.py
+++ digital_product/settings.py
@@ -35,7 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    
+    'rest_framework',
+    
     'products.apps.ProductsConfig',
 ]
 
@@ -127,3 +129,11 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# REST_FRAMEWORK = {
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+#     ]
+# }
