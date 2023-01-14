e32f0f76877f5e4698fb0288dc9e25b7f5ac3dbc
javad <javad.feyzabadi@yahoo.com>
2023-01-14 14:50:33 +0330

    add

--- digital_product/urls.py
+++ digital_product/urls.py
@@ -1,5 +1,4 @@
-"""digital_product URL Configuration
-
+"""digital_products URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -14,16 +13,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
-    path('api-auth/', include('rest_framework.urls')),
-    path('', include('users.urls')),
+    path('', include('users.urls'))
 ]
 
 if settings.IS_DEVEL:
-    urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
